1acf06fcaad7e8af6800f308398147433a2b33e5
James Appello <jamesappello@gmail.com>
2019-08-05 12:08:10 -0400

    set some psudoCode in javascript file for when we begin the nittyGrity

--- assets/javascript/meetU-app.js
+++ assets/javascript/meetU-app.js
@@ -1,7 +1,24 @@
-$(document).ready(function() {
+// INSTRUCTIONS
+//1. establish globalVariables
+//2. onclick function for the initMeetU button
+    //input for name and zipcode
+    //show the map
+//2. add meetU-people
+    //names?
+    //where do they live? 
+    
+
+
+
+// BEGIN JAVASCRIPT //
+#(document).ready(function() {
 
 
 
 
   
 })
+    
+
+
+    
