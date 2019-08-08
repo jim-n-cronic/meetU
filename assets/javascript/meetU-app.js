0d5a4fb55aefb41c839f1cb5255dec33439aaf40
James Appello <jamesappello@gmail.com>
2019-08-07 21:19:42 -0400

    progress1; may just redo the whole thing in the SAME method as described in class, but instead of starting witht the form I'm going to Instead just start with a modal and from there go to a form page, becuase I realize Im getting stuck on one thing which is an animation troubleshooting that I dont want to waste time, that we dont have, over trying to fix when we have a full app to build

--- assets/javascript/meetU-app.js
+++ assets/javascript/meetU-app.js
@@ -1,22 +1,59 @@
 // INSTRUCTIONS
-//1. establish globalVariables
-//2. onclick function for the initMeetU button
-    //input for name and zipcode
-    //show the map
-//2. add meetU-people
-    //names?
-    //where do they live? 
-    
-
 
 
 // BEGIN JAVASCRIPT //
-#(document).ready(function() {
+$(document).ready(function() {
+    // establish globalVariables
+    var pplArray = [];
+
+    $("#user-submitInit").on('click', function(addFrndMdL) {
+     /* TMGChgd/~\ &&('addScreen' var.remove) //  
+      $("#start-screen").hide(); 
+      */
+     //make info dissappear
+     //not sure why shake effect wont work.
+     //$("#screenDelta").effect('shake');
+     $(".initInputInfo").hide();
+
+        //cout >> modal to ELIF(addFriend) endl;
+        //initConsoleLog
+        console.log($(document.modal));
+        console.log($(document.modal).css("hidden"));
+        addFrndMdL = $(".addFrndModal");
+
+        
+        //unCmt This when ready **//addFrndMdL.show();
+        
+
+     //make form bigger
+     
+
+        
+        /* ~~~~~~
+         CSS-REMINDER: to make new classes/ids for new screens 
+         moving thru the app-Functionality!
+        //~~~~~~~~~~~*/
+        
+
+       /*THIS MIGHT GET TAKEN OUT.//
+       /////////////
+        addScreen = $("<div>").addClass("container");
+        console.log(addScreen);
+        console.log("$(#addFriends)");
+        \/\/\/\/\/\/\/\/\/\///\/\/\/\/\*/
+        //at THIS POINT the div is 'created' but it is NOT-shown at this time!
+        //append addScreen to the body WHEN FINSHED CODE*//
 
 
 
 
-  
+       
+       
+
+    })
+
+
+
 })
     
 
