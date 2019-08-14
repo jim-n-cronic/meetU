df337540620a27fcd601d3728822f086fe8fc4e3
Jack Dempsey <jdempsey1993@gmail.com>
2019-08-14 18:05:26 -0400

    Added calendar code  into main UI

--- assets/javascript/meetU-app.js
+++ assets/javascript/meetU-app.js
@@ -124,6 +124,13 @@ $(document).ready(function() {
             
             
         })
-    
+        $( function() {
+            $( "#datepicker" ).datepicker({
+              inline: true,
+                firstDay: 1,
+                showOtherMonths: true,
+                dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+            });
+          } );
 })
 
